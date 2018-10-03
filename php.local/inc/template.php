3351a6f89ab5a4fdbe16ed3d057be26f465a26d9
kgythu <42789263+kgythu@users.noreply.github.com>
2018-10-03 19:49:32 +0200

    The favicon'll never change :-)

--- php.local/inc/template.php
+++ php.local/inc/template.php
@@ -4,12 +4,8 @@
 	<title><?=$page_title?> • <?=SITE_NAME?></title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="shortcut icon" href="/favicon.ico?ver=<?php
-		echo filemtime('favicon.ico');
-	?>" />
-	<link rel="icon" href="/favicon.ico?ver=<?php
-		echo filemtime('favicon.ico');
-	?>" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="icon" href="/favicon.ico" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	<!--[if gte IE 9]> -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
